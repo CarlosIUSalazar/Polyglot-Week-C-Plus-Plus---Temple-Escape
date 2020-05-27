c1a1a95bb4b94d3da9aa6f41d1293009bbdd54de
Carlos Salazar <carlosiusalazar@gmail.com>
2020-05-28 08:58:31 +0900

    Grabber and Ray Tracing implemented

---
+++ Source/Temple_Escape/Grabber.cpp
@@ -0,0 +1,81 @@
+// Copyright Carlos Salazar - For Polyglot Week Code Chrysalis
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "Grabber.h"
+
+#define OUT
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation, 
+		OUT PlayerViewPointRotation
+	); //OUT parameters, Out is a keyowrd we deffined outselves near the header, it doesnt do anyuthing is only for readibility
+
+	// UE_LOG(LogTemp, Warning, TEXT("Location: %s Rotation: %s"), 
+	// *PlayerViewPointLocation.ToString(),
+	// *PlayerViewPointRotation.ToString()
+	// ); //%s is string
+
+	//Draw a line from player showing the reach
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(0,255,0),
+		false,
+		0.f,
+		0,
+		5.f
+	);
+
+	FHitResult Hit;
+	//ray cast out to a certain distance
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+
+	//See what it hits
+	AActor* ActorHit = Hit.GetActor();
+	if(ActorHit){
+		UE_LOG(LogTemp, Error, TEXT("Line trace has hit: %s"), *(ActorHit -> GetName()))
+	}
+
+
+}
+
